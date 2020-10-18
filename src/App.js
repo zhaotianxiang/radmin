391cc97017d001a8c2f07e42c991e0e3ddc41ad1
zhaotianxiang <904001993@qq.com>
2020-10-18 17:38:58 +0800

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import Empty from './view/Empty';
+import Login from './view/Login';
+import Home from './view/Home';
+
+import './App.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact render={(props)=>{
+          return <Redirect to="/login"></Redirect>;
+        }}></Route>
+
+        <Route path="/home" render={(props)=>{
+          return <Home {...props}></Home>
+        }}></Route>
+
+        <Route path="/login" component={Login}></Route>
+        
+        <Route component={Empty}></Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
+
